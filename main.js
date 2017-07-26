c584f414629d43e3fcf50c3f42e1a77f2eadd110
Keri Spencer <kerigspencer@gmail.com>
2017-07-26 09:43:49 -0400

    Unfinished but getting close. Need to complete styling.

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ const data = require('./data')
 const mustacheExpress = require('mustache-express')
 
 const app = express()
+app.use(express.static('public'))
 
-app.get('/', (res, req) => {})
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', './templates')
+
+app.get('/', (req, res) => {
+  res.render('home', data)
+})
+
+app.get('/info/:id', (req, res) => {
+  let id = req.params.id
+  id = parseInt(id)
+  res.render('info', data.users[id - 1])
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
